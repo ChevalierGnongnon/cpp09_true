5c999679b2106848ce11d33d6579babede12fe91
chhoflac <chhoflac@student.42.fr>
2025-10-09 10:52:58 +0200

    exceptions made

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,7 +6,69 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 14:39:51 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/10/07 14:39:52 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/10/09 10:52:30 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "RPN.hpp"
+
+RPN::RPN(){
+	
+}
+
+RPN::RPN(const std::string &entry) : expression(entry){
+	
+}
+
+RPN::RPN(const RPN &src){
+	this->digits = src.digits;
+}
+
+RPN &RPN::operator=(const RPN &src){
+	if (this != &src)
+		this->digits = src.digits;
+	return (*this);
+}
+RPN::~RPN(){
+	
+}
+
+bool					RPN::isValidDigit(long long &out) const{
+
+}
+
+bool					RPN::pushIfDigit(const std::string &tok){
+	
+}
+
+
+size_t					RPN::digitCount() const{
+	
+}
+
+long long				RPN::makeOperation(int a, int b, char sign){
+	
+}
+bool					RPN::isOperator(char c) const{
+	
+}
+
+const char				*RPN::DivisionByZeroException::what() const throw(){
+	return ("Error : Division by 0.");
+}
+
+const char				*RPN::NoDigitsException::what() const throw(){
+	return ("Error : No digits in this expression.");
+}
+
+const char				*RPN::NoOperatorException::what() const throw(){
+	return ("Error : No operators in this expression.");
+}
+
+const char				*RPN::TooHighDigitException::what() const throw(){
+	return ("Error : digits accepted (only 0 - 9).");
+}
+
+const char				*RPN::InvalidInputException::what() const throw(){
+	return ("Error : invalid input(only 0 - 9 and + - / *).");
+}
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,56 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 14:39:54 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/10/07 14:39:55 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/10/08 15:15:49 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stack>
+#include <exception>
+#include <string>
+
+class RPN{
+	private :
+		std::string				expression;
+		std::stack<long long>	digits;
+		static long long		makeOperation(long long a, long long b, char sign);
+		static bool				isOperator(char c);
+	public :
+		RPN();
+		RPN(const std::string &entry);
+		RPN(const RPN &src);
+		RPN &operator=(const RPN &src);
+		~RPN();
+
+		bool					isValidDigit(const std::string &entry, long long &out) const;
+		bool					isValidEntry(const std::string &entry);
+		bool					pushIfDigit(const std::string &tok);
+		size_t					digitCount() const;
+
+		
+		
+		long long				evaluate();
+		
+		class DivisionByZeroException : public std::exception{
+			public :
+				virtual const char *what() const throw();
+		};
+		
+		class NoDigitsException : public std::exception{
+			public :
+				virtual const char *what() const throw();
+		};
+		
+		class NoOperatorException : public std::exception{
+			public :
+				virtual const char *what() const throw();
+		};
+		class TooHighDigitException : public std::exception{
+			public :
+				virtual const char *what() const throw();
+		};
+		class InvalidInputException : public std::exception{
+			public :
+				virtual const char *what() const throw();
+		};
+};
