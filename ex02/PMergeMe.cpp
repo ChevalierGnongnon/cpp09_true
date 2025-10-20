7540293f9892bf5f8ed74d435014dbe40b42edcc
chhoflac <chhoflac@student.42.fr>
2025-10-20 16:49:18 +0200

    done

--- ex02/PMergeMe.cpp
+++ ex02/PMergeMe.cpp
@@ -6,7 +6,73 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 14:08:47 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/10/15 14:59:24 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/10/20 16:25:17 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "PMergeMe.hpp"
+
+PMergeMe::PMergeMe(){
+	
+}
+PMergeMe::PMergeMe(const std::string &values){
+	this->input = values;
+	if (this->checkInput()){
+		fillVector();
+		fillDeque();	
+	}
+}
+
+PMergeMe::PMergeMe(const PMergeMe &src){
+	this->input = src.input;
+	this->vect = src.vect;
+	this->cont = src.cont;
+}
+PMergeMe &PMergeMe::operator=(const PMergeMe &src){
+	if (this != &src){
+		this->input = src.input;
+		this->vect = src.vect;
+		this->cont = src.cont;
+	}
+	return (*this);
+}
+PMergeMe::~PMergeMe(){
+	
+}
+
+bool				PMergeMe::checkInput(){
+	for (int i = 0; i < this->input.size(); i++){
+		if (!isspace(this->input[i]) && (this->input[i] <= '0' || this->input[i] >= '9'))
+			throw (PMergeMe::InvalidInputException());
+	}
+}
+
+int 				PMergeMe::getValue(size_t *i){
+	std::string	strValue;
+	size_t		start = (*i);
+	int			value;
+	
+	while ((*i) < this->input.size() && !isspace(this->input[(*i)]))
+		(*i)++;
+	strValue = this->input.substr(start, (*i));
+	if (strValue == "")
+		throw (PMergeMe::InvalidInputException());
+	value = std::atoi(strValue.c_str());
+	return (value);
+}
+
+void				PMergeMe::fillVector(){
+	
+}
+
+void				PMergeMe::fillDeque(){
+	
+}
+
+void				PMergeMe::sortVect(){
+	
+}
+void				PMergeMe::sortDeque(){
+	
+}
 
--- ex02/PMergeMe.hpp
+++ ex02/PMergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 14:08:51 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/10/15 14:59:28 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/10/20 16:10:18 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,23 @@
 # include <iostream>
 # include <vector>
 # include <deque>
+# include <cctype>
 
 class PMergeMe{
 	private :
 		std::vector<int>	vect;
 		std::deque<int>		cont;
-		std::string	input;
+		std::string			input;
 	public :
 		PMergeMe();
-		PMergeMe(const std::string &input);
+		PMergeMe(const std::string &values);
 		PMergeMe(const PMergeMe	&src);
 		PMergeMe &operator=(const PMergeMe &src);
 		~PMergeMe();
 
-		void				checkInput();
+		bool				checkInput();
+		
+		int 				getValue(size_t *i);
 		
 		void				fillVector();
 		void				fillDeque();
