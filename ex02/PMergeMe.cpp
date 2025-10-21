0808eb9c76d7122ee6e792669caf74fe32b88cea
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-10-21 14:51:29 +0200

    adding input parsing and vector filling

--- ex02/PMergeMe.cpp
+++ ex02/PMergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 14:08:47 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/10/20 16:25:17 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/10/21 14:50:51 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ PMergeMe::PMergeMe(const std::string &values){
 		fillVector();
 		fillDeque();	
 	}
+	else
+		throw (PMergeMe::InvalidInputException());
 }
 
 PMergeMe::PMergeMe(const PMergeMe &src){
@@ -41,10 +43,17 @@ PMergeMe::~PMergeMe(){
 }
 
 bool				PMergeMe::checkInput(){
+	int n = 0;
+	
 	for (int i = 0; i < this->input.size(); i++){
 		if (!isspace(this->input[i]) && (this->input[i] <= '0' || this->input[i] >= '9'))
-			throw (PMergeMe::InvalidInputException());
+			return (false);
+		else
+			n++;
 	}
+	if (n == 0)
+		return (false);
+	return (true);
 }
 
 int 				PMergeMe::getValue(size_t *i){
@@ -62,7 +71,17 @@ int 				PMergeMe::getValue(size_t *i){
 }
 
 void				PMergeMe::fillVector(){
+	size_t	i = 0;
+	size_t	j = 0;
 	
+	while (i < this->input.size()){
+		if (this->input[i] >= '0' && this->input[i] <= '9'){
+			this->vect[j] = getValue(&i);
+			j++;
+			continue ;
+		}
+		i++;
+	}
 }
 
 void				PMergeMe::fillDeque(){
